2945131e05b1191746f9651b630144408391e357
Eliel Sousa <sousaeliel@gmail.com>
2021-10-06 10:46:46 -0300

    refactor: add resolvers for relations

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,15 @@
 package graph
 
+import (
+	"github.com/sousaeliel/go-graphql/graph/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Categories []*model.Category
+	Courses []*model.Course
+	Chapters []*model.Chapter	
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@
 
 ## Na criação do projeto
 - go run github.com/99designs/gqlgen init
+
+## Com o projeto já criado
+- go run github.com/99designs/gqlgen generate
+
+## Resolvendo o problema de N+1
+- DataLoader => Nesta lib utilize o Query Resolution / Resolvers para resolver este tipo de problema
